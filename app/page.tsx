6a8e1c17247f95b71eaa32e216b5667306b0f588
taiti09 <taitiabe1997@gmail.com>
2023-11-24 22:14:02 +0900

    fix : error

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,331 @@
-import { Box, Heading, Text } from "./common/components";
+import {
+  Box,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  Link,
+  Text,
+} from "./common/components";
+import { Monster, Title, monsters, titles } from "./lib/const";
+import Image from "next/image";
+
+interface Pos {
+  x: number;
+  y: number;
+}
 
 export default function Home() {
+  // リストラ経験者
+  const target1 = monsters.filter((monster) => {
+    return titles.some((title) => monster[title.key as keyof Monster] === 0);
+  });
+
+  // 皆勤賞組
+  const isKaikins = monsters.filter((item) => !target1.includes(item));
+
+  // MHW以降のモンスター
+  const NewMonsters = monsters.filter((monster) => {
+    return titles.some(
+      (title) =>
+        monster["MHW" as keyof Monster] === 2 ||
+        monster["MHWI" as keyof Monster] === 2 ||
+        monster["MHR" as keyof Monster] === 2 ||
+        monster["MHRS" as keyof Monster] === 2
+    );
+  });
+
+  // MHX以降のモンスター
+  const NewMonsters2 = monsters.filter((monster) => {
+    return titles.some(
+      (title) =>
+        monster["MHW" as keyof Monster] === 2 ||
+        monster["MHWI" as keyof Monster] === 2 ||
+        monster["MHR" as keyof Monster] === 2 ||
+        monster["MHRS" as keyof Monster] === 2 ||
+        monster["MHX" as keyof Monster] === 2 ||
+        monster["MHXX" as keyof Monster] === 2
+    );
+  });
+
+  // MH3以降のモンスター
+  const NewMonsters3 = monsters.filter((monster) => {
+    return titles.some(
+      (title) =>
+        monster["MHW" as keyof Monster] === 2 ||
+        monster["MHWI" as keyof Monster] === 2 ||
+        monster["MHR" as keyof Monster] === 2 ||
+        monster["MHRS" as keyof Monster] === 2 ||
+        monster["MHX" as keyof Monster] === 2 ||
+        monster["MHXX" as keyof Monster] === 2 ||
+        monster["MH4" as keyof Monster] === 2 ||
+        monster["MH4G" as keyof Monster] === 2 ||
+        monster["MH3G" as keyof Monster] === 2 ||
+        monster["MH3" as keyof Monster] === 2 ||
+        monster["P3rd" as keyof Monster] === 2
+    );
+  });
+
+  // 忘れ去られた組
+  const target2 = monsters.filter((monster) => {
+    const zeroValueKeyCount = titles.reduce((count, title) => {
+      return monster[title.key as keyof Monster] === 0 ? count + 1 : count;
+    }, 0);
+
+    return zeroValueKeyCount >= 15;
+  });
+  const restructuries = target2.filter((item) => !NewMonsters2.includes(item));
+
+  // さらに忘れ去られた組
+  const target3 = monsters.filter((monster) => {
+    const zeroValueKeyCount = titles.reduce((count, title) => {
+      return monster[title.key as keyof Monster] === 0 ? count + 1 : count;
+    }, 0);
+
+    return zeroValueKeyCount >= 15;
+  });
+  const restructuries2 = target2.filter((item) => !NewMonsters3.includes(item));
+
+  // 大体いる
+  const almostTitles = target1.filter((monster) => {
+    const NotzeroValueKeyCount = titles.reduce((count, title) => {
+      return monster[title.key as keyof Monster] !== 0 ? count + 1 : count;
+    }, 0);
+
+    return NotzeroValueKeyCount >= 12;
+  });
+
   return (
-    <main>
+    <Box bg={"gray.200"}>
       {/* ヘッダー */}
-      <Box>
+      <Box display={"flex"} flexDirection={"row"} p={4}>
+        <Image
+          src={"/logo-mh20th.png"}
+          alt="ロゴ画像"
+          height={96}
+          width={100}
+        />
         <Heading
           as={"h1"}
-          bgColor={"gray.500"}
-          color={"white"}
           textAlign={"center"}
           w={"100%"}
           h={"10vh"}
+          fontSize={{ base: "20px", md: "30px" }}
+          justifyContent={"center"}
         >
-          Hello クソアプリ
+          モンハン登場回数ランキング
         </Heading>
       </Box>
 
       {/* コンテンツ */}
-      <Box
-        bgColor={"green.500"}
-        color={"white"}
-        textAlign={"center"}
-        w={"100%"}
-        minH={"80vh"}
-      >
+      <Box color={"black"} textAlign={"center"} w={"100%"} minH={"80vh"}>
+        {/* 忘れ去られた組 */}
+        <Heading as={"h2"} marginY={16}>
+          😭 忘れ去られた組
+        </Heading>
+        <Text marginY={4}>残念。。。😭😭😭</Text>
+        <Text marginY={4}>最終登場作品は、 MHP2ndG 以前です。</Text>
+        <Box w={"80%"} minH={"30vh"} m={"auto"} alignItems={"center"}>
+          <Grid
+            templateColumns={{ base: "repeat(2, 1fr)", md: "repeat(3, 1fr)" }}
+            gap={1}
+          >
+            <>
+              {restructuries2.map((item) => (
+                <GridItem
+                  color={"white"}
+                  w={"100%"}
+                  key={item.index}
+                  h={"200px"}
+                  display={"flex"}
+                  flexDirection={"column"}
+                  justifyContent={"center"}
+                  alignItems={"center"}
+                  bg={"blue.700"}
+                  borderRadius={"8px"}
+                  padding={4}
+                  border={"1px"}
+                  cursor={"pointer"}
+                >
+                  <Text textAlign={"center"} fontWeight={"bold"}>
+                    {item.name.ja}
+                  </Text>
+                  <Image
+                    src={`/${item.index}.png`}
+                    alt={`モンスター画像_${item.index}`}
+                    width={100}
+                    height={100}
+                  />
+                </GridItem>
+              ))}
+            </>
+          </Grid>
+        </Box>
+
         <Box
-          w={"50%"}
-          minH={"30vh"}
-          position={"absolute"}
+          w={"80%"}
+          border={"1px solid gray"}
           m={"auto"}
-          alignItems={"center"}
-          bgColor={"white"}
-          top={"25%"}
-          left={"25%"}
-        >
-          コンテンツ
+          marginY={"5rem"}
+        ></Box>
+
+        {/* 皆勤賞組 */}
+        <Heading as={"h2"} marginY={16}>
+          ⭐️ 皆勤賞組
+        </Heading>
+        <Text marginY={16}>さすがのリオ夫婦❗️</Text>
+        <Box w={"80%"} minH={"30vh"} m={"auto"} alignItems={"center"}>
+          <Grid
+            templateColumns={{ base: "repeat(2, 1fr)", md: "repeat(3, 1fr)" }}
+            gap={1}
+          >
+            <>
+              {isKaikins.map((item) => (
+                <GridItem
+                  color={"white"}
+                  w={"100%"}
+                  key={item.index}
+                  h={"200px"}
+                  display={"flex"}
+                  flexDirection={"column"}
+                  justifyContent={"center"}
+                  alignItems={"center"}
+                  bg={"orange.700"} // 木箱の色に合わせて変更
+                  borderRadius={"8px"} // 木箱の角を丸くする
+                  padding={4} // 適宜調整
+                  border={"1px"}
+                >
+                  <Text textAlign={"center"} fontWeight={"bold"}>
+                    {item.name.ja}
+                  </Text>
+                  <Image
+                    src={`/${item.index}.png`}
+                    alt={`モンスター画像_${item.index}`}
+                    width={100}
+                    height={100}
+                  />
+                </GridItem>
+              ))}
+            </>
+          </Grid>
+        </Box>
+
+        <Box
+          w={"80%"}
+          border={"1px solid gray"}
+          m={"auto"}
+          marginY={"5rem"}
+        ></Box>
+
+        {/* 準皆勤賞組 */}
+        <Heading as={"h2"} marginY={4}>
+          😄 だいたいの作品に登場しているイツメン
+        </Heading>
+        <Text marginY={16}>
+          フルフルって実は、モンハンの１７作品中、１２作品に登場している古参かつよくいるモンスターなんです
+        </Text>
+        <Box w={"80%"} minH={"30vh"} m={"auto"} alignItems={"center"}>
+          <Grid
+            templateColumns={{ base: "repeat(2, 1fr)", md: "repeat(3, 1fr)" }}
+            gap={1}
+          >
+            <>
+              {almostTitles.map((item) => (
+                <GridItem
+                  color={"white"}
+                  w={"100%"}
+                  key={item.index}
+                  h={"200px"}
+                  display={"flex"}
+                  flexDirection={"column"}
+                  justifyContent={"center"}
+                  alignItems={"center"}
+                  bg={"green.700"} // 木箱の色に合わせて変更
+                  borderRadius={"8px"} // 木箱の角を丸くする
+                  padding={4} // 適宜調整
+                  border={"1px"}
+                >
+                  <Text textAlign={"center"} fontWeight={"bold"}>
+                    {item.name.ja}
+                  </Text>
+                  <Image
+                    src={`/${item.index}.png`}
+                    alt={`モンスター画像_${item.index}`}
+                    width={100}
+                    height={100}
+                  />
+                </GridItem>
+              ))}
+            </>
+          </Grid>
+        </Box>
+
+        <Box
+          w={"80%"}
+          border={"1px solid gray"}
+          m={"auto"}
+          marginY={"5rem"}
+        ></Box>
+
+        {/* 新参組  MHW:I以降　*/}
+        <Heading as={"h2"} marginY={4}>
+          ⭐️ 新参組
+        </Heading>
+        <Box w={"80%"} minH={"30vh"} m={"auto"} alignItems={"center"}>
+          <Grid
+            templateColumns={{ base: "repeat(2, 1fr)", md: "repeat(3, 1fr)" }}
+            gap={1}
+          >
+            <>
+              {NewMonsters.map((item) => (
+                <GridItem
+                  color={"white"}
+                  w={"100%"}
+                  key={item.index}
+                  h={"200px"}
+                  display={"flex"}
+                  flexDirection={"column"}
+                  justifyContent={"center"}
+                  alignItems={"center"}
+                  bg={"green.700"} // 木箱の色に合わせて変更
+                  borderRadius={"8px"} // 木箱の角を丸くする
+                  padding={4} // 適宜調整
+                  border={"1px"}
+                >
+                  <Text textAlign={"center"} fontWeight={"bold"}>
+                    {item.name.ja}
+                  </Text>
+                  <Image
+                    src={`/${item.index}.png`}
+                    alt={`モンスター画像_${item.index}`}
+                    width={100}
+                    height={100}
+                  ></Image>
+                </GridItem>
+              ))}
+            </>
+          </Grid>
         </Box>
       </Box>
 
+      <Box
+        w={"80%"}
+        border={"1px solid gray"}
+        m={"auto"}
+        marginY={"5rem"}
+      ></Box>
+
       {/* フッター */}
-      <Box>
-        <Text
-          bgColor={"yellow.500"}
-          color={"white"}
-          textAlign={"center"}
-          w={"100%"}
-          h={"10vh"}
-        >
-          フッター
-        </Text>
-      </Box>
-    </main>
+      <Flex
+        flexDirection={{ base: "column", xl: "row" }}
+        alignItems={"center"}
+        justifyContent={"center"}
+        h={{ base: "20vh", xl: "20vh" }}
+      >
+        <Flex flexDirection={"row"}></Flex>
+
+        <Text marginLeft={"30px"}>&copy; copyright 2023 o-ga09</Text>
+      </Flex>
+    </Box>
   );
 }
