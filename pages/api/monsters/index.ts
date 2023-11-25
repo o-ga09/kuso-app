8eb0b4588a235aec1787569bee7c883a8e90bd7d
taiti09 <taitiabe1997@gmail.com>
2023-11-25 18:24:52 +0900

    fix : API Route

--- pages/api/monsters/index.ts
+++ pages/api/monsters/index.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import fs from "fs";
+import { Monster } from '../../../app/lib/const';
 
 type ResponseData = {
   message: string
@@ -9,6 +10,10 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  const articles = JSON.parse(fs.readFileSync("public/Monsters.json", "utf8"));
-  res.status(200).json(articles);
+  const queryParamArrray = Object.values(req.query);
+  const monsters = JSON.parse(fs.readFileSync("public/Monsters.json", "utf8"));
+  const filterdMonsters = monsters["monsters"].filter((monster:any) => {
+    return queryParamArrray.some((title) => monster[title as keyof Monster] !== 0);
+  });
+  res.status(200).json(filterdMonsters);
 }
